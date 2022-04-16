53aa533af440ee6e0893842c30e587ddd6c1876f
feynoobs <p.w.feeniej@gmail.com>
2022-04-17 02:49:07 +0900

    コメント

--- lib/camera.dart
+++ lib/camera.dart
@@ -26,12 +26,15 @@ class _CameraState extends State<Camera>
     {
         super.initState();
         availableCameras().then((cameras) {
+            // 最初に見つかった背面カメラを使用する
             CameraDescription? camera;
             for (var element in cameras) {
                 if (element.lensDirection == CameraLensDirection.back) {
                     camera = element;
+                    break;
                 }
             }
+            // 背面カメラがなかったらアラートを出す
             if (camera == null) {
                 showDialog(
                     context: context,
@@ -45,14 +48,19 @@ class _CameraState extends State<Camera>
                     )
                 );
             }
+            // 背面カメラがあれば処理継続
             else {
                 _controller = CameraController(camera, ResolutionPreset.max);
+                // initializeがカメラ起動処理完了待ちらしい
                 _controller!.initialize().then((_) {
+                    // 画像読み込み
                     _controller?.startImageStream((CameraImage image) async {
+                        // OCR処理中はスキップ
                         if (_isRunning == false) {
                             _isRunning = true;
-                            _text = '';
                             final RecognisedText rt = await GoogleMlKit.vision.textDetector().processImage(_createInputImageFromCameraImage(image));
+                            // とりあえず認識したテキストを全部つなげてみる
+                            _text = '';
                             for (TextBlock block in rt.blocks) {
                                 for (TextLine line in block.lines) {
                                     for (TextElement elem in line.elements) {
@@ -61,6 +69,9 @@ class _CameraState extends State<Camera>
                                 }
                             }
                             _logger.v(_text);
+                            // 認識したテキストをsetStateで描画する
+                            // 画面が終わったあとにOCRの処理が戻ってくることがあるので
+                            // mountedでチェックする
                             if (mounted == true) {
                                 setState(() {});
                             }
@@ -77,9 +88,11 @@ class _CameraState extends State<Camera>
     {
         Widget preview;
 
+        // カメラ起動前はとりあえずコンテナ表示する
         if (_controller == null) {
             preview = Container();
         }
+        // カメラ起動後はプレビューを表示する
         else {
             preview = CameraPreview(_controller!);
         }
@@ -113,6 +126,9 @@ class _CameraState extends State<Camera>
         super.dispose();
     }
 
+    // CameraImageをOCRが処理できるInputImageに変換するメソッド
+    // https://pub.dev/documentation/google_ml_kit/latest/
+    // にあるソースをほぼコピペ.ぶっちゃけ全くわからない
     InputImage _createInputImageFromCameraImage(CameraImage cameraImage)
     {
         final WriteBuffer buffer = WriteBuffer();
